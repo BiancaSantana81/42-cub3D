b0f3503c243a473067e099e83698e835a66fe996
bda-mota <bea.mota04@gmail.com>
2024-08-02 17:52:21 -0300

    add file draw.c on makefile

--- includes/graphic.h
+++ includes/graphic.h
@@ -7,9 +7,6 @@
 int32_t		key_pressed(mlx_key_data_t keydata, keys_t key1, keys_t key2);
 void		hook_key_press(mlx_key_data_t keydata, void *param);
 void		hook_close(void *param);
-void		draw_example_pixel(t_cub *game);
-void		draw_grid(t_cub *game);
-void    	draw_line(mlx_image_t *image, int x0, int y0, int x1, int y1, uint32_t color);
 void		calculate_window_size(t_cub *game);
 
 /* IMAGES */
