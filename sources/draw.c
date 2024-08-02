c7ac8f90b1de42b5294bf61b5269ee876392fe58
bda-mota <bea.mota04@gmail.com>
2024-08-02 17:24:57 -0300

    draw_map

--- sources/draw.c
+++ sources/draw.c
@@ -1,5 +1,7 @@
 #include "../includes/cub.h"
 
+static void	draw_map_square(t_cub *game, int start_x, int start_y, int size, uint32_t color);
+
 void	draw_square(t_cub *game, int start_x, int start_y, int size, uint32_t color)
 {
 	uint32_t x;
@@ -17,9 +19,10 @@ void	draw_square(t_cub *game, int start_x, int start_y, int size, uint32_t color
 		}
 		y++;
 	}
+	mlx_image_to_window(game->mlx, game->mlx_image, 0, 0);
 }
 
-void draw_player_square(t_cub *game)
+void	draw_player_square(t_cub *game)
 {
 	int	start_x;
 	int	start_y;
@@ -34,4 +37,43 @@ void draw_player_square(t_cub *game)
 		draw_square(game, start_x, start_y, size, color);
 }
 
+void	draw_map(t_cub *game)
+{
+	int i;
+	int j;
 
+	i = 0;
+	while (i < game->data->lines)
+	{
+		j = 0;
+		while (j < game->data->columns)
+		{
+			if (game->data->map[i][j] == '1')
+				draw_map_square(game, j * BLOCK, i * BLOCK, BLOCK, 0x00FFFFFF);
+			else if (game->data->map[i][j] == '0')
+				draw_map_square(game, j * BLOCK, i * BLOCK, BLOCK, 0x00000000);
+			j++;
+		}
+		i++;
+	}
+	mlx_image_to_window(game->mlx, game->map_image, 0, 0); 
+}
+
+static void	draw_map_square(t_cub *game, int start_x, int start_y, int size, uint32_t color)
+{
+	uint32_t x;
+	uint32_t y;
+
+	y = (uint32_t)start_y;
+	while (y < (uint32_t)start_y + size)
+	{
+		x = (uint32_t)start_x;
+		while (x < (uint32_t)start_x + size)
+		{
+			if (x < game->map_image->width && y < game->map_image->height)
+				mlx_put_pixel(game->map_image, x, y, color);
+			x++;
+		}
+		y++;
+	}
+}
