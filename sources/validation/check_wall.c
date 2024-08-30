938440ee9e867036889532331dd75e45a960edde
bda-mota <bea.mota04@gmail.com>
2024-08-30 11:53:31 -0300

    add fill_map_wwith_two

--- sources/validation/check_wall.c
+++ sources/validation/check_wall.c
@@ -12,25 +12,20 @@
 
 #include "../../includes/cub.h"
 
+static void fill_map_with_twos(t_data *data);
+
 bool	check_diagonals(t_data *data, int line, int col)
 {
 	char	**map;
 
 	map = data->map;
-	if (line > 0 && col > 0 && col < (int)ft_strlen(map[line - 1])
-		&& (map[line - 1][col - 1] == ' ' || map[line - 1][col - 1] == '\n'))
+	if (map[line - 1][col - 1] == ' ' || map[line - 1][col - 1] == '\n')
 		return (false);
-	else if (line < data->lines - 1 && col > 0
-		&& col < (int)ft_strlen(map[line + 1])
-		&& (map[line + 1][col - 1] == ' ' || map[line + 1][col - 1] == '\n'))
+	else if (map[line + 1][col - 1] == ' ' || map[line + 1][col - 1] == '\n')
 		return (false);
-	else if (line > 0 && col < (int)ft_strlen(map[line]) - 1
-		&& col < (int)ft_strlen(map[line - 1])
-		&& (map[line - 1][col + 1] == ' ' || map[line - 1][col + 1] == '\n'))
+	else if (map[line - 1][col + 1] == ' ' || map[line - 1][col + 1] == '\n')
 		return (false);
-	else if (line < data->lines - 1 && col < (int)ft_strlen(map[line]) - 1
-		&& col < (int)ft_strlen(map[line + 1])
-		&& (map[line + 1][col + 1] == ' ' || map[line + 1][col + 1] == '\n'))
+	else if (map[line + 1][col + 1] == ' ' || map[line + 1][col + 1] == '\n')
 		return (false);
 	return (true);
 }
@@ -40,17 +35,13 @@ bool	check_sides(t_data *data, int line, int col)
 	char	**map;
 
 	map = data->map;
-	if (line > 0 && col < (int)ft_strlen(map[line - 1])
-		&& (map[line - 1][col] == ' ' || map[line - 1][col] == '\n'))
+	if (map[line - 1][col] == ' ' || map[line - 1][col] == '\n')
 		return (false);
-	else if (line < data->lines - 1 && col < (int)ft_strlen(map[line + 1])
-		&& (map[line + 1][col] == ' ' || map[line + 1][col] == '\n'))
+	else if (map[line + 1][col] == ' ' || map[line + 1][col] == '\n')
 		return (false);
-	else if (col > 0
-		&& (map[line][col - 1] == ' ' || map[line][col - 1] == '\n'))
+	else if (map[line][col - 1] == ' ' || map[line][col - 1] == '\n')
 		return (false);
-	else if (col < (int)ft_strlen(map[line]) - 1
-		&& (map[line][col + 1] == ' ' || map[line][col + 1] == '\n'))
+	else if (map[line][col + 1] == ' ' || map[line][col + 1] == '\n')
 		return (false);
 	return (true);
 }
@@ -63,6 +54,7 @@ void	surrounded_by_walls(t_data *data)
 	int		width;
 
 	y = 0;
+	fill_map_with_twos(data);
 	map = data->map;
 	while (map[y])
 	{
@@ -81,4 +73,34 @@ void	surrounded_by_walls(t_data *data)
 		}
 		y++;
 	}
+	handle_error("Error: invalid map, check the walls.\n");
+}
+
+static void fill_map_with_twos(t_data *data)
+{
+	int		i;
+	int		j;
+	int		len;
+	char	**new_map;
+	
+	i = 0;
+	new_map = ft_calloc(data->lines + 1, sizeof(char *));
+	while (i < data->lines)
+	{
+		new_map[i] = ft_calloc((data->columns + 2), sizeof(char));
+		len = strlen(data->map[i]);
+		ft_strncpy(new_map[i], data->map[i], len);
+		j = len;
+		printf("antes: %s\n", new_map[i]);
+		while (j < data->columns)
+		{
+			new_map[i][j] = '2';
+			j++;
+		}
+		printf("depois: %s\n", new_map[i]);
+		new_map[i][j] = '\0';
+		i++;
+	}
+	ft_free_matrix(data->map);
+	data->map = new_map;
 }
